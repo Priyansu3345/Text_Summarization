983abc289d348ffe3c00f394769838fe4faf50d7
Priyansu3345 <139443422+Priyansu3345@users.noreply.github.com>
2023-11-27 20:51:24 +0530

    Update Text_Summarizer.py

--- Text_Summarizer.py
+++ Text_Summarizer.py
@@ -1,47 +1,59 @@
-import nltk
-from nltk.corpus import stopwords
-from nltk.tokenize import word_tokenize, sent_tokenize
-from flask import Flask, redirect, url_for, request ,render_template
+from flask import Flask, render_template, url_for, request
+import time
+import spacy
+from spacy.lang.en.stop_words import STOP_WORDS
+from string import punctuation
+from heapq import nlargest
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
+
+nlp = spacy.load("en_core_web_sm")
 app = Flask(__name__)
+
+
+def txt_summarizer(raw_docx):
+    stopwords = list(STOP_WORDS)
+    raw_text = raw_docx
+    docx = nlp(raw_text)
+
+   
+    word_frequency = {}
+    for word in docx:
+        if word.text not in stopwords:
+            if word.text not in word_frequency.keys():
+                word_frequency[word.text] = 1
+            else:
+                word_frequency[word.text] += 1
+    maximum_frequncy = max(word_frequencies.values())
+    for word in word_frequency.keys():
+        word_frequency[word] = (word_frequency[word]/maximum_frequncy)
+    
+    
+    sentence_list = [sentence for sentence in docx.sents]
+
+    
+    sentence_scores = {}
+    for sent in sentence_list:
+        for word in sent:
+            if word.text.lower() in word_frequencies.keys():
+                if len(sent.text.split(' ')) < 30:
+                    if sent not in sentence_scores.keys():
+                        sentence_scores[sent] = word_frequencies[word.text.lower()]
+                    else:
+                        sentence_scores[sent] += word_frequencies[word.text.lower()]
+
+    summarized_sentences = nlargest(7, sentence_scores, key=sentence_scores.get)
+    final_sentences = [w.text for w in summarized_sentences]
+    summary = ' '.join(final_sentences)
+
+    return summary
+
+
+def readingT(mytext):
+    total_words = len([token.text for token in nlp(mytext)])
+    estimatedTime = total_words/200.0
+    return estimatedTime
 
 @app.route('/')
 def index():
-    return render_template("index.html")
-@app.route('/output',methods=['POST'])
-def output():
-    if request.method == 'POST':
-        textvalue = request.form.get("textarea", None)
-        return render_template('Output_Content.html', res=outputsumm(textvalue))
-if __name__ == '__main__':
-    app.run(debug=True)
-
-def outputsumm(input_text):
-	stopWords = set(stopwords.words("english"))
-	words = word_tokenize(input_text)
-	f = dict()
-	for i in words:
-		i = i.lower()
-		if i in stopWords:
-			continue
-		if i in f:
-			f[i] += 1
-		else:
-			f[i] = 1
-	sentences = sent_tokenize(input_text)
-	fsent = dict()
-	for j in sentences:
-		for x, y in f.items():
-			if x in j.lower():
-				if j in fsent:
-					fsent[j] += y
-				else:
-					fsent[j] = y
-	count = 0
-	for k in fsent:
-		count += fsent[k]
-	average = int(count / len(fsent))
-	output_text = ''
-	for p in sentences:
-		if (p in fsent) and (fsent[p] > (1.2 * average)):
-			output_text += " "+p
-	return output_text
+    return render_template('index.html')
