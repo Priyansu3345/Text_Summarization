0fbda6637bd747654f7801abb2c2cec6f95ff9c9
Priyansu3345 <139443422+Priyansu3345@users.noreply.github.com>
2023-11-27 20:44:23 +0530

    Update Text_Summarizer.py

--- Text_Summarizer.py
+++ Text_Summarizer.py
@@ -7,6 +7,11 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template("index.html")
+@app.route('/output',methods=['POST'])
+def output():
+    if request.method == 'POST':
+        textvalue = request.form.get("textarea", None)
+        return render_template('Output_Content.html', res=outputsumm(textvalue))
 if __name__ == '__main__':
     app.run(debug=True)
 
